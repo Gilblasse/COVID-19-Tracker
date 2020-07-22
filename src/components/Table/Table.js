71eac09440f1f6bd26235f21cc638d2518f1e8a2
Nethelbert Blasse <gilblasse@gmail.com>
2020-07-22 18:34:04 -0400

    added additional components

---
+++ src/components/Table/Table.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Table.css'
+
+
+function Table({countries}) {
+
+    const sortCountriesByCases = ()=>{
+        return countries.sort((countryA,countryB) => {
+            return countryA.cases - countryB.cases
+        }).reverse()
+    }
+
+
+
+
+    return (
+        <div className="table">
+            
+            {
+                sortCountriesByCases().map(({country,cases}) =>(
+                    <tr>
+                        <td>{country}</td>
+                        <td><strong>{cases}</strong></td>
+                    </tr>
+                ))
+            }
+
+        </div>
+    )
+}
+
+export default Table
