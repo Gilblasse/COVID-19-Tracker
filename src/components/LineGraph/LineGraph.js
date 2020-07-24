e30d28d36f4683b29e5aec360b2b0230eb906193
nethelbert <gilblasse@gmail.com>
2020-07-24 19:41:52 -0400

    built chart data with options and added it to Line Chart

--- src/components/LineGraph/LineGraph.js
+++ src/components/LineGraph/LineGraph.js
@@ -3,7 +3,7 @@ import { Line } from "react-chartjs-2";
 import { historicalURL } from "../../constants/APIS";
 import numeral from "numeral";
 
-function LineGraph() {
+export default function LineGraph() {
   const [data, setData] = useState({});
 
   useEffect(() => {
@@ -13,18 +13,84 @@ function LineGraph() {
   const getHistoricalData = async () => {
     const res = await fetch(historicalURL);
     const historicalData = await res.json();
-    console.log(numeral(10000.23).format("+0,0"));
-    setData(historicalData);
+    const chartData = buildChartData(historicalData);
+
+    setData(chartData);
   };
 
-  const buildChartData = () => {};
+  const buildChartData = (data, caseType = "cases") => {
+    let chartData = [];
+    let prevDateValue;
 
-  console.log("Data: ", data);
+    Object.entries(data[caseType]).forEach(([date, value]) => {
+      prevDateValue && chartData.push({ x: date, y: value - prevDateValue });
+      prevDateValue = value;
+    });
+
+    return chartData;
+  };
+
   return (
     <div>
-      <Line />
+      {data?.length > 0 && (
+        <Line
+          options={options}
+          data={{
+            datasets: [
+              {
+                backgroundColor: "rgba(204, 16, 52, 0.5)",
+                borderColor: "#CC1034",
+                data: data,
+              },
+            ],
+          }}
+        />
+      )}
     </div>
   );
 }
 
-export default LineGraph;
+const options = {
+  legend: {
+    display: false,
+  },
+  elements: {
+    point: {
+      radius: 0,
+    },
+  },
+  maintainAspectRatio: false,
+  tooltips: {
+    mode: "index",
+    intersect: false,
+    callbacks: {
+      label: function (tooltipItem, data) {
+        return numeral(tooltipItem.value).format("+0,0");
+      },
+    },
+  },
+  scales: {
+    xAxes: [
+      {
+        type: "time",
+        time: {
+          format: "MM/DD/YY",
+          tooltipFormat: "ll",
+        },
+      },
+    ],
+    yAxes: [
+      {
+        gridLines: {
+          display: false,
+        },
+        ticks: {
+          // Include a dollar sign in the ticks
+          callback: function (value, index, values) {
+            return numeral(value).format("0a");
+          },
+        },
+      },
+    ],
+  },
+};
