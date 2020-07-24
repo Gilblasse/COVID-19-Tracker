fa02294adc71566d8a592a088a3bbd35347a72a6
nethelbert <gilblasse@gmail.com>
2020-07-24 19:21:32 -0400

    imported numeral js

--- src/components/LineGraph/LineGraph.js
+++ src/components/LineGraph/LineGraph.js
@@ -1,35 +1,30 @@
-import React, {useState, useEffect} from 'react'
-import { Line } from 'react-chartjs-2';
-import {historicalURL} from '../../constants/APIS'
-
-
+import React, { useState, useEffect } from "react";
+import { Line } from "react-chartjs-2";
+import { historicalURL } from "../../constants/APIS";
+import numeral from "numeral";
 
 function LineGraph() {
-    const [data , setData] = useState({})
+  const [data, setData] = useState({});
 
-    useEffect(() => {
-        getHistoricalData()
-    }, [])
+  useEffect(() => {
+    getHistoricalData();
+  }, []);
 
+  const getHistoricalData = async () => {
+    const res = await fetch(historicalURL);
+    const historicalData = await res.json();
+    console.log(numeral(10000.23).format("+0,0"));
+    setData(historicalData);
+  };
 
-    const getHistoricalData = async ()=>{
-        const res = await fetch(historicalURL)
-        const historicalData = await res.json()
+  const buildChartData = () => {};
 
-        setData(historicalData)
-    }
-
-    const buildChartData = ()=>{
-        
-    }
-
-    console.log('Data: ',data)
-    return (
-        <div>
-            <Line 
-            />
-        </div>
-    )
+  console.log("Data: ", data);
+  return (
+    <div>
+      <Line />
+    </div>
+  );
 }
 
-export default LineGraph
+export default LineGraph;
